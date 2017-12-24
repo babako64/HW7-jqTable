84f13918e77f1b202e7b0c9de2ce9661dc10bc31
babak <babako64@gmail.com>
2017-12-25 01:09:32 +0330

    commit3

---
+++ WebContent/js/gettable.js
@@ -0,0 +1,52 @@
+jQuery.ajax({
+            url: "http://localhost:8080/HW7-jqTable/api/students",
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function(resultData) {
+            	console.log(resultData);
+            	
+            	drawTable(resultData);
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+            },
+
+        });
+
+function drawTable(data) {
+	
+    for (var i = 0; i < data.length; i++) {
+        drawRow(data[i]);
+    }
+    $('#table1').Tabledit({
+   	    url: 'http://localhost:8080/HW7-jqTable/api/students',
+   	    eventType: 'dblclick',
+   	   
+   	    columns: {
+   	        identifier: [0, 'id'],
+   	        editable: [[1, 'name'], [2, 'dept'], [3, 'superVisorId']]
+   	    },
+   	    
+    
+   	});
+    
+    var row =$("<tr id='form1'><td><input type='text' class='form-control' id='id' placeholder='id'></td>" +
+    		"<td><input type='text' class='form-control' id='name' placeholder='name'></td>" +
+    		"<td><input type='text' class='form-control' id='dept' placeholder='dept'></td>" +
+    		"<td><input type='text' class='form-control' id='sup' placeholder='supervisorid'></td>" +
+    		"<td><input class='btn btn-success' type='button' value='Add'id='btn-add'></td>" +
+    		"</tr>");
+    $( "tr" ).first().after(row);
+}
+
+function drawRow(rowData) {
+    var row = $("<tr />")
+ 
+    $("#table1").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    row.append($("<td>" + rowData.id + "</td>"));
+    row.append($("<td>" + rowData.name + "</td>"));
+    row.append($("<td>" + rowData.dept + "</td>"));
+    row.append($("<td>" + rowData.superVisorId + "</td>"));
+   
+}
+
---
+++ src/ir/maktab/api/Students.java
@@ -0,0 +1,78 @@
+package ir.maktab.api;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import ir.maktab.DAO.StudentDAOImp;
+import ir.maktab.model.Student;
+
+@Path("/students")
+public class Students {
+
+StudentDAOImp stDAO = new StudentDAOImp();
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void addStudent(Student st) {
+		
+		stDAO.add(st);
+	}
+	
+	@DELETE
+	@Path("/{stid}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void delete(@PathParam("stid") String stid) {
+		System.out.println(stid);
+		stDAO.remove(Integer.parseInt(stid));
+		
+	}
+	
+	@DELETE
+	public void delete() {
+		
+		stDAO.removeAll();
+		
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void update(Student st,@PathParam("id") String id) {
+		
+		stDAO.Update(st.getId(), st.getName(), st.getDept(), st.getSuperVisorId());
+		
+	}
+	
+	@GET
+	//@Path("/{id}")
+	//@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public  ArrayList<Student> get(@QueryParam("id")String id) {
+		
+		ArrayList<Student> stList = new ArrayList<>();
+		Student st;
+		stList = stDAO.getStudent(Integer.parseInt(id));
+		st = stList.get(0);
+		System.out.println(st.getId() + st.getName() + st.getDept() + st.getSuperVisorId());
+		return stList;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Student> get() {
+		ArrayList<Student> stList = new ArrayList<>();
+		//Student st;
+		stList =stDAO.getAll();
+		return stList;
+	}
+}
